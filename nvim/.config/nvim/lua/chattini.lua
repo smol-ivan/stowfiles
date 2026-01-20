8a4408d71e4c0b815c15f79271d6bd525dcee119
Ivan Gordillo <gordillosolis@icloud.com>
2026-01-19 22:04:44 -0600

    add nvim tree

--- nvim/.config/nvim/lua/chattini.lua
+++ nvim/.config/nvim/lua/chattini.lua
@@ -38,7 +38,6 @@ vim.o.winborder = "rounded"
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 map("n", "<Esc>", "<cmd>nohlsearch<CR>", opts)
-map("n", "<leader>e", "<Cmd>Ex<CR>", opts, { desc = "Open Explorer" })
 map("n", "<leader>ts", ":Themery<CR>", opts, { desc = "[T]heme [S]witcher" })
 map("v", "J", ":m '>+1<CR>gv=gv", opts, { desc = "Move block down" })
 map("v", "K", ":m '<-2<CR>gv=gv", opts, { desc = "Move block up" })
@@ -54,7 +53,7 @@ map("n", "<leader>w3", "<Cmd>BufferGoto 3<CR>", opts)
 map("n", "<leader>w4", "<Cmd>BufferGoto 4<CR>", opts)
 map("n", "<leader>w5", "<Cmd>BufferGoto 5<CR>", opts)
 map("n", "<leader>wc", "<Cmd>BufferClose<CR>", opts)
-
+map("n", "<Leader>e", ":NvimTreeToggle<CR>", opts, { desc = "Toggle nerdtree" })
 local function toggle_diagnostics()
     if vim.diagnostic.is_enabled() then
         vim.diagnostic.disable()
@@ -66,3 +65,7 @@ local function toggle_diagnostics()
 end
 
 vim.keymap.set("n", "<leader>d", toggle_diagnostics, { desc = "Toggle Virtual Text" })
+
+-- map("n", "<leader>e", "<Cmd>Ex<CR>", opts, { desc = "Open Explorer" })
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
