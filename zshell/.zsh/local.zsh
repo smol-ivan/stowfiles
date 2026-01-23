1fee572193805027466cd1d23dbebc1848c5e7cd
Ivan Gordillo <gordillosolis@icloud.com>
2026-01-22 19:07:07 -0600

    zshell: go back to emacs style UuU

--- zshell/.zsh/local.zsh
+++ zshell/.zsh/local.zsh
@@ -3,7 +3,7 @@ if [[ -f "/opt/homebrew/bin/brew" ]] then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-bindkey -v
+bindkey -e
 export KEYTIMEOUT=1
 
 zinit ice depth=1; zinit light romkatv/powerlevel10k
@@ -17,5 +17,6 @@ zinit light Aloxaf/fzf-tab
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
+
 
 
