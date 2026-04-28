593cc1fdbff5905a8931dc0e035e58e00b0bbfaa
Ivan Gordillo <gordillosolis@icloud.com>
2026-04-27 18:25:08 -0600

    Hope it dont break something docker releated outside macos

--- zshell/.zsh/env.zsh
+++ zshell/.zsh/env.zsh
@@ -2,4 +2,5 @@ export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
 [[ -f "$HOME/.cargo/env" ]] && . "$HOME/.cargo/env"
 
-export PATH=$PATH:/usr/local/go/bin
+# export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/.docker/bin:$PATH
