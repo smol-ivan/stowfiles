de2176c0fb1bbce27e2bd5a057fd6d9263df4044
Ivan Gordillo <gordillosolis@icloud.com>
2026-08-26 14:49:47 -0600

    Update(hyprland): up to date hyprland

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -16,7 +16,7 @@ hl.monitor({
 hl.monitor({
     output = "HDMI-A-1",
     mode = "1920x1080@60.00",
-    position = "auto-up",
+    position = "auto-right",
     scale = "1.25",
 })
 
@@ -44,7 +44,7 @@ local clipboard =
     "cliphist list | rofi -dmenu -display-columns 2 -p 'Clipboard' -theme .config/rofi/launchers/type-1/style-3.rasi | cliphist decode | wl-copy"
 local waybar = "killall -9 waybar || waybar &"
 local da_script = "/home/cherry/.local/bin/change_wallpaper"
-local llogout = "wlogout"
+local llogout = "wlogout -n -P 1"
 local da_ss = "bash -c \"grim -g '$(slurp)' /home/cherry/Pictures/$(date +'%Y-%m-%d_%H-%M-%S').png\""
 local kanata = "/home/cherry/.local/bin/da_layout"
 
@@ -56,8 +56,9 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
     hl.exec_cmd("awww-daemon")
     hl.exec_cmd("waybar")
-    hl.exec_cmd("hyprctl setcursor Imouto 30")
+    hl.exec_cmd("hyprctl setcursor miku_theme 32")
     hl.exec_cmd("hypridle")
+    hl.exec_cmd("hyprsunset")
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
 end)
 
@@ -67,8 +68,9 @@ end)
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XCURSOR_SIZE", "30")
-hl.env("HYPRCURSOR_SIZE", "30")
+hl.env("XCURSOR_SIZE", "32")
+hl.env("HYPRCURSOR_SIZE", "32")
+hl.env("HYPRCURSOR_THEME", "miku_theme")
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 
@@ -117,37 +119,9 @@ hl.config({
     },
 
     animations = {
-        enabled = true,
+        enabled = false,
     },
 })
-
--- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
-hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
-hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
-hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
-hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
-hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
-
--- Default springs
-hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
-
-hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
-hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
-hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
-hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, spring = "easy", style = "popin 87%" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, bezier = "linear", style = "popin 87%" })
-hl.animation({ leaf = "fadeIn", enabled = true, speed = 1.73, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeOut", enabled = true, speed = 1.46, bezier = "almostLinear" })
-hl.animation({ leaf = "fade", enabled = true, speed = 3.03, bezier = "quick" })
-hl.animation({ leaf = "layers", enabled = true, speed = 3.81, bezier = "easeOutQuint" })
-hl.animation({ leaf = "layersIn", enabled = true, speed = 4, bezier = "easeOutQuint", style = "fade" })
-hl.animation({ leaf = "layersOut", enabled = true, speed = 1.5, bezier = "linear", style = "fade" })
-hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 1.79, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1.39, bezier = "almostLinear" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesIn", enabled = true, speed = 1.21, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "zoomFactor", enabled = true, speed = 7, bezier = "quick" })
 
 hl.config({
     dwindle = {
@@ -160,13 +134,6 @@ hl.config({
         new_status = "master",
     },
 })
-
--- Infinite tape
--- hl.config({
---     scrolling = {
---         fullscreen_on_one_column = true,
---     },
--- })
 
 ----------------
 ----  MISC  ----
@@ -187,19 +154,11 @@ hl.config({
     input = {
         kb_layout = "us",
         kb_variant = "colemak_dh_wide",
-        kb_model = "",
-        kb_options = "",
-        kb_rules = "",
 
         follow_mouse = 0,
 
         sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
     },
-})
-
-hl.device({
-    name = "epic-mouse-v1",
-    sensitivity = -0.5,
 })
 
 ---------------------
@@ -214,8 +173,10 @@ hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd(terminal))
 hl.bind(mainMod .. " + Q", hl.dsp.window.close())
 -- KANATA INIT
 hl.bind(mainMod .. " + SHIFT + Z", hl.dsp.exec_cmd(kanata))
--- LOGOUT
-hl.bind(mainMod .. " + SHIFT + e", hl.dsp.exec_cmd(llogout))
+-- lock session
+hl.bind(mainMod .. " + SHIFT + L", hl.dsp.exec_cmd("hyprlock"))
+-- logout
+hl.bind(mainMod .. " + L",  hl.dsp.exit())
 -- ROTATE WALLPAPER
 hl.bind(mainMod .. " + SHIFT + W", hl.dsp.exec_cmd(da_script))
 -- RESTART WAYBAR
@@ -236,15 +197,32 @@ hl.bind(mainMod .. " + T", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit")) -- dwindle only
 
--- Move focus with mainMod + arrow keys
-hl.bind(mainMod .. " + left", hl.dsp.focus({ direction = "left" }))
-hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
-hl.bind(mainMod .. " + up", hl.dsp.focus({ direction = "up" }))
-hl.bind(mainMod .. " + down", hl.dsp.focus({ direction = "down" }))
+-- Focus
 hl.bind(mainMod .. " + h", hl.dsp.focus({ direction = "left" }))
 hl.bind(mainMod .. " + l", hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + k", hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + j", hl.dsp.focus({ direction = "down" }))
+
+hl.bind(
+    mainMod .. " + XF86AudioRaiseVolume",
+    hl.dsp.exec_cmd("hyprctl hyprsunset gamma +10"),
+    { repeating = true, locked = true }
+)
+hl.bind(
+    mainMod .. " + XF86AudioLowerVolume",
+    hl.dsp.exec_cmd("hyprctl hyprsunset gamma -10"),
+    { repeating = true, locked = true }
+)
+hl.bind(
+    mainMod .. " + SHIFT + XF86AudioRaiseVolume ",
+    hl.dsp.exec_cmd("hyprctl hyprsunset temperature +500"),
+    { repeating = true, locked = true }
+)
+hl.bind(
+    mainMod .. " + SHIFT + XF86AudioLowerVolume",
+    hl.dsp.exec_cmd("hyprctl hyprsunset temperature -500"),
+    { repeating = true, locked = true }
+)
 
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
@@ -253,10 +231,6 @@ for i = 1, 10 do
     hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
     hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i, follow = false }))
 end
-
--- Scroll through existing workspaces with mainMod + scroll
-hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
-hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
 
 -- Move/resize windows with mainMod + LMB/RMB and dragging
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
@@ -278,19 +252,6 @@ hl.bind(
     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
     { locked = true, repeating = true }
 )
-hl.bind(
-    "XF86AudioMicMute",
-    hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
-    { locked = true, repeating = true }
-)
-hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
-
--- Requires playerctl
-hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
@@ -303,7 +264,7 @@ local suppressMaximizeRule = hl.window_rule({
 
     suppress_event = "maximize",
 })
--- suppressMaximizeRule:set_enabled(false)
+suppressMaximizeRule:set_enabled(false)
 
 hl.window_rule({
     -- Fix some dragging issues with XWayland
