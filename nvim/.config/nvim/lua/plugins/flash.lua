68e01d7a1007d06fd68d06e6b0c685bce209d230
Ivan Gordillo <gordillosolis@icloud.com>
2026-09-11 14:36:33 -0600

    Fix(nvim): url path

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,5 +1,8 @@
 vim.pack.add({
-    src = "folke/flash.nvim",
+    { src = "https://github.com/folke/flash.nvim" },
+})
+
+require("flash").setup({
     opts = {
         jump = { nohlsearch = true },
         prompt = {
